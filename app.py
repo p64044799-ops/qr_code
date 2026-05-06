b4796f8bdd911a44dd3348f7e6026d8dbbad74c3
p64044799-ops <p64044799@gmail.com>
2026-05-06 15:19:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-if st.button("Generate QR"):
-if data:
-# All these lines must be aligned perfectly
-qr = qrcode.make(data)
-qr.save("qr.png")
-img = Image.open("qr.png")
-with open("qr.png", "rb") as f:
-st.download_button("Download QR", f, file_name="qr.png")
-else:
-st.warning("Please enter some text")
-
-
+import streamlit as st 
+import qrcode 
+from PIL import Image 
+st.title("Qr code generator") 
+data=st.text_input("enter url") 
+if st.button("Generate QR"): 
+if data: 
+  qrqrcode.make(data) 
+  qr.save("qr.png") 
+  img = Image.open("qr.png") 
+  with open("qr.png", "rb") as f: 
+    st.download_button("Download QR", f, file_name="qr.png")
+else: 
+  st.warning("Please enter some text")
